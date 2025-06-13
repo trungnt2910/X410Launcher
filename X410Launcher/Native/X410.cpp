4c233d4963f88a805751523935b2001934adead6
Trung <57174311+trungnt2910@users.noreply.github.com>
2025-06-14 03:34:59 +1000

    feat: Initial ARM64 support
    Now X410 v4 can launch as a native ARM64 process without depending on
    the Microsoft Store.

--- X410Launcher/Native/X410.cpp
+++ X410Launcher/Native/X410.cpp
@@ -30,13 +30,21 @@ int WINAPI HookMessageBoxW(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uT
 
         SymInitializeW(GetCurrentProcess(), NULL, TRUE);
 
-#ifdef _M_X64
+#if defined(_M_X64)
         machine = IMAGE_FILE_MACHINE_AMD64;
         frame.AddrPC.Offset    = context.Rip;
         frame.AddrPC.Mode      = AddrModeFlat;
         frame.AddrFrame.Offset = context.Rbp;
         frame.AddrFrame.Mode   = AddrModeFlat;
         frame.AddrStack.Offset = context.Rsp;
+        frame.AddrStack.Mode   = AddrModeFlat;
+#elif defined(_M_ARM64)
+        machine = IMAGE_FILE_MACHINE_ARM64;
+        frame.AddrPC.Offset    = context.Pc;
+        frame.AddrPC.Mode      = AddrModeFlat;
+        frame.AddrFrame.Offset = context.Fp;
+        frame.AddrFrame.Mode   = AddrModeFlat;
+        frame.AddrStack.Offset = context.Sp;
         frame.AddrStack.Mode   = AddrModeFlat;
 #else
 #error Fill in frame info for this architecture!
@@ -66,7 +74,7 @@ int WINAPI HookMessageBoxW(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uT
         {
             IsFirstMessage = FALSE;
 
-#ifdef _M_X64
+#if defined(_M_X64)
             __asm__ volatile(
                 "mov %0, %%rbx;"
                 // "mov %1, %%rbp;"
@@ -89,18 +97,42 @@ int WINAPI HookMessageBoxW(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uT
 
                 "jmp *%%rcx;"
                 :
-                : "m" (context.Rbx),
-                "m" (context.Rbp),
-                "m" (context.Rsi),
-                "m" (context.Rdi),
-                "m" (context.R12),
-                "m" (context.R13),
-                "m" (context.R14),
-                "m" (context.R15),
+                :   "m" (context.Rbx),
+                    "m" (context.Rbp),
+                    "m" (context.Rsi),
+                    "m" (context.Rdi),
+                    "m" (context.R12),
+                    "m" (context.R13),
+                    "m" (context.R14),
+                    "m" (context.R15),
 
-                "m" (context.Rsp),
-                "m" (context.Rip)
+                    "m" (context.Rsp),
+                    "m" (context.Rip)
                 :
+            );
+#elif defined(_M_ARM64)
+            __asm__ volatile(
+                "ldr     x19, [%0, #0xa0]\n\t"
+                "ldr     x20, [%0, #0xa8]\n\t"
+                "ldr     x21, [%0, #0xb0]\n\t"
+                "ldr     x22, [%0, #0xb8]\n\t"
+                "ldr     x23, [%0, #0xc0]\n\t"
+                "ldr     x24, [%0, #0xc8]\n\t"
+                "ldr     x25, [%0, #0xd0]\n\t"
+                "ldr     x26, [%0, #0xd8]\n\t"
+                "ldr     x27, [%0, #0xe0]\n\t"
+                "ldr     x28, [%0, #0xe8]\n\t"
+
+                "ldr     x29, [%0, #0xf0]\n\t"
+                "ldr     x30, [%0, #0xf8]\n\t"
+
+                "mov     x0, #0x1        \n\t"
+                "mov     sp, %2          \n\t"
+                "br      %1              \n\t"
+                :
+                : "r"(&context), "r"(context.Pc), "r"(context.Sp)
+                : "x19", "x20", "x21", "x22", "x23", "x24",
+                  "x25", "x26", "x27", "x28", "x29", "x30"
             );
 #else
 #error Restore context for this architecture!
